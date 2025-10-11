75e79c70bc2e4a79693abe4db069c15def1b744b
moruedee-F <monruedi2006@gmail.com>
2025-10-11 15:54:13 +0700

    lab7-8

---
+++ react-app/src/features/Home.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import Product from './Product';
+import Addform from './Product/Addform';
+
+let currentProductId = 9;
+
+export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    function addProduct(product) {
+      const newProduct = { id: ++currentProductId, ...product };
+      setProducts([...products, newProduct]);
+    }
+
+    async function getProducts() {
+      const products = await axios.get(
+        'https://68e9f962f1eeb3f856e5959c.mockapi.io/data'
+      );
+      setProducts(products.data);
+    }
+
+    getProducts();
+
+    return (
+      <>
+      <h1>New Products</h1>
+      {
+        products.length > 0 ? (
+          <ul className="Home__products">
+            {products.map((product) => (
+              <Product key={product.id} item={product} />
+            ))}
+          </ul>
+        ) : (
+          <div>Loading products....</div>
+        )
+      }
+      <Addform addProduct={addProduct} />
+    </>
+    )
+}
