6dc2a233e19edb10823957c232649aa2c22ad799
moruedee-F <monruedi2006@gmail.com>
2025-10-19 10:29:00 +0700

    lab11

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,47 @@ import Navbar from './features/Navbar';
 import Container from './features/Container';
 import Home from './features/Home';
 import GlobalStyle from './features/GlobalStyle';
+import { Route, Routes } from 'react-router-dom';
+import AddForm from './features/Product/Addform';
+import UpdateForm from './features/Product/UpdateForm';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+let currentProductId = 9;
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const products = await axios.get(
+        'https://68e9f962f1eeb3f856e5959c.mockapi.io/data'
+      );
+      setProducts(products.data);
+    }
+    getProducts();
+  }, []);
+
+  function addProduct(product) {
+    const newProduct = { id: currentProductId++, ...product };
+    setProducts([...products, newProduct]);
+  }
+
   return (
     <>
-    <GlobalStyle />
+      <GlobalStyle />
       <Navbar />
       <Container>
-        <Home />
+        {products.length > 0 ? (
+          <Routes>
+            <Route path="/create-product" element={<AddForm addProduct={addProduct} />} />
+            <Route path="/update-product/:id" element={<UpdateForm />} />
+            <Route path="/" element={<Home products={products} />} />
+          </Routes>
+        ) : (
+          <div>Loading products....</div>
+        )}
       </Container>
     </>
   );
--- react-app/src/features/Home.js
+++ react-app/src/features/Home.js
@@ -1,50 +1,20 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
-import Product from './Product';
-import Addform from './Product/Addform';
-import { useEffect } from 'react';
-
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-let currentProductId = 9;
+import Product from './Product';
 
-function Home() {
-    const [products, setProducts] = useState([]);
-
-    function addProduct(product) {
-      const newProduct = { id: ++currentProductId, ...product };
-      setProducts([...products, newProduct]);
-    }
-
-    useEffect(() => {
-    async function getProducts() {
-      const products = await axios.get(
-        'https://68e9f962f1eeb3f856e5959c.mockapi.io/data'
-      );
-      setProducts(products.data);
-    }
-
-    getProducts();
-    }, []);
-
-    return (
-      <>
+function Home({ className, products }) {
+  return (
+    <div className={className}>
       <h1>New Products</h1>
-      {
-        products.length > 0 ? (
-          <ul className="Home__products">
-            {products.map((product) => (
-              <Product key={product.id} item={product} />
-            ))}
-          </ul>
-        ) : (
-          <div>Loading products....</div>
-        )
-      }
-      <Addform addProduct={addProduct} />
-    </>
-    )
+      <ul className="Home__products">
+        {products.map((product) => (
+          <Product key={product.id} item={product} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default styled(Home)`
